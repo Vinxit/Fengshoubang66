9c3fd0f3a21830b10215b784f2400c9b2108093b
Vinxit <Vinxit_HK@163.com>
2017-03-27 21:58:29 +0800

    功能更新提交2

---
+++ app/src/main/java/com/mingpin/fengshoubang/utils/JsonUtils.java
@@ -0,0 +1,28 @@
+package com.mingpin.fengshoubang.utils;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * Json转换工具类
+ * Created by Administrator on 2017/3/23.
+ */
+
+public class JsonUtils {
+    private static Gson mGson = new Gson();
+    /**
+     * 将json字符串转换为对象
+     */
+    public static <T>T deserialize(String json, Class<T> clz)throws JsonSyntaxException{
+        return mGson.fromJson(json,clz);
+    }
+
+    /**
+     * 将json对象转换为实体对象
+     */
+    public static <T> T deserialize(JsonObject json,Class<T> clz)throws JsonSyntaxException{
+        return mGson.fromJson(json,clz);
+    }
+}
